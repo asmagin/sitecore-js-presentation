a9ac20e3bad8d72ddf3fcb866d2cbc317baa7256
Alex Smagin <asmagin@users.noreply.github.com>
2017-09-25 10:00:24 -0400

    Add constructor parameter for serialization settings. (#11)

--- src/Sitecore.Js.Presentation/Component.cs
+++ src/Sitecore.Js.Presentation/Component.cs
@@ -70,6 +70,7 @@ namespace Sitecore.Js.Presentation
             string componentName,
             object model,
             Rendering rendering,
+            JsonSerializerSettings serializerSetting = null,
             string site = null,
             string containerId = null,
             string containerTag = null,
@@ -86,7 +87,7 @@ namespace Sitecore.Js.Presentation
             this.ContainerTag = string.IsNullOrEmpty(containerTag) ? "div" : containerTag;
             this.ContainerClass = containerClass;
 
-            this._jsonSerializerSettings = new JsonSerializerSettings();
+            this._jsonSerializerSettings = serializerSetting ?? new JsonSerializerSettings(); 
         }
 
         /// <summary>
